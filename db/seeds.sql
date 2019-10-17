5cfbd31fb30e949096857e4bd978290aec2e8686
lambertco <blambertco@gmail.com>
2019-10-16 20:03:27 -0600

    buttons not working, still debugging

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Gunslinger', false);
+INSERT INTO burgers (name, devoured) VALUES ('All The Cheeses', false);
+INSERT INTO burgers (name, devoured) VALUES ('Butcher Burger', true);
